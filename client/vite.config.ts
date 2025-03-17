67a23227351824388ad715a921f8c0ada17eacfb
phiiiiiiii <philsyvmartin13@gmail.com>
2025-03-17 12:50:44 +0530

    corrected tsconfig files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -26,7 +26,7 @@ export default defineConfig({
       '@containers': path.resolve(__dirname, 'src/containers'),
       '@config': path.resolve(__dirname, 'src/config'),
 
-      
+
     },
   },
 
